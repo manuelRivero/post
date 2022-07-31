5963d25b191369370ce7bf0f03efc3a1ebb6a1fa
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2022-07-31 12:53:13 -0300

    [refactor] button componen

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -7,23 +7,16 @@ import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/cor
 })
 
 export class ButtonComponent implements OnInit, OnChanges {
-  types:any = {
-    primary: 'primary',
-    secondary: 'secondary',
-    danger: 'danger',
-    success: 'success',
-  }
+ 
   @Input() type:string = ""; 
-  buttonClasses:string;
   constructor() { 
-    this.buttonClasses = `btn btn-${this.types[this.type]}`;
   }
 
   ngOnInit(): void {
   }
   ngOnChanges(changes: SimpleChanges) {
     if(changes['type']){
-      this.buttonClasses = `btn btn-${this.types[this.type]}`;
+      this.type = changes['type'].currentValue;
     }
   }
 }
