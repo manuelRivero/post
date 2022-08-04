73d3e5ba488b1137007ceacad76ba40008a9648b
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2022-08-04 20:23:12 -0300

    [add] header dropdown outoclose event

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('toggleButton') toggleButton!: ElementRef;
+  
+  isOpen: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private renderer: Renderer2) {
+    this.renderer.listen('window', 'click', (e: Event) => {
+      if (e.target !== this.toggleButton.nativeElement) {
+        this.isOpen = false;
+      }
+    });
   }
 
+  ngOnInit(): void {}
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+  }
 }
