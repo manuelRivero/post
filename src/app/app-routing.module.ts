647f8a24b6a1648aed6d10875a8009156ee57bee
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2023-02-20 19:32:31 -0300

    [wip] angular universal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,8 +28,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      initialNavigation: 'enabledBlocking',
-    }),
+    initialNavigation: 'enabledBlocking',
+}),
   ],
   exports: [RouterModule],
 })
